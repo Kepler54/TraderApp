bbabac58c1eef92555b25ca5d3bdb06f0cd99da3
Kepler <keplersemail@proton.me>
2023-08-29 23:15:09 +0200

    2. Exceptions have been added

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ trade = DataAnalysis()
 
 def main() -> None:
     """Entry point"""
-    trade.verify_pair_file()
-    trade.add_db_value()
-    print(trade.get_full_info())
+    try:
+        trade.verify_pair_file()
+        trade.add_db_value()
+        print(trade.get_full_info())
+    except ValueError:
+        print("В базе данных осутствуют необходимые значения!")
+    except SyntaxError:
+        print("В файле coin_pair осутствуют необходимые значения!")
+    except KeyboardInterrupt:
+        print("Вы ничего не ответили!")
 
 
 if __name__ == '__main__':
