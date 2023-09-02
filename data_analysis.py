0f2ecc7e2d77fb4ddcfae0a0b61a5790eb15894a
Kepler <keplersemail@proton.me>
2023-09-02 15:55:01 +0200

    6. New functions buy_sell_verify and percentage have been added

--- data_analysis.py
+++ data_analysis.py
@@ -5,7 +5,7 @@ class DataAnalysis(DataBase):
     def coin_trade_info(self, buy_or_sell) -> list:
         """
         The function divides currency pairs accepted as a tuple into separate buy and sell lists
-        :param buy_or_sell: int
+        :param buy_or_sell: object
         :return: str
         """
         values_list = self.get_value_list("SELECT buy, sell FROM trades")
@@ -14,24 +14,46 @@ class DataAnalysis(DataBase):
             value_list.append(float(value[buy_or_sell]))
         return value_list
 
+    def percentage(self, value, buy_or_sell) -> list:
+        """
+        The function returns the percentage ratio
+        :param value: list
+        :param buy_or_sell: object
+        :return: list
+        """
+        return (
+            f'{float(f"{value / self.coin_trade_info(buy_or_sell)[0]:%}"[0:-1]) - 100:.8f}',
+            f'{value / self.coin_trade_info(buy_or_sell)[0]:%}'
+        )
+
+    def buy_sell_verify(self) -> str:
+        """
+        The function returns the decision to buy or sell the coin
+        :return: str
+        """
+        if float(self.percentage(self.coin_trade_info(self.buy)[-1], self.buy)[0]) >= 1.5:
+            return f", нужно покупать [{self.get_coin_name(self.coin_second)}]"
+        elif float(self.percentage(self.coin_trade_info(self.buy)[-1], self.buy)[0]) <= -1.5:
+            return f", нужно продавать [{self.get_coin_name(self.coin_second)}]"
+        else:
+            return ''
+
     def get_exchange_info(self, value, buy_or_sell) -> str:
         """
         The function takes a list consisting of coin values and returns the percentage ratio
         of the last element of the list to the first one
         :param value: list
-        :param buy_or_sell: int
+        :param buy_or_sell: object
         :return: str
         """
-        percent = f'{value / self.coin_trade_info(buy_or_sell)[0]:%}'
-
         if len(self.coin_trade_info(buy_or_sell)) != 1:
-            if f'{float(percent[0:-1]) - 100}'[0:5][0] == '-':
+            if f'{float(self.percentage(value, buy_or_sell)[1][0:-1]) - 100}'[0:5][0] == '-':
                 return (
                     f"Изменение стоимости [{self.get_coin_name(self.coin_first)}] по отношению к "
                     f"[{self.get_coin_name(self.coin_second)}] {self.coin_trade_info(buy_or_sell)[-1]} "
-                    f"составило: {float(percent[0:-1]) - 100:.8f}% ↓"
+                    f"составило: {float(self.percentage(value, buy_or_sell)[1][0:-1]) - 100:.8f}% ↓"
                 )
-            elif float(percent[0:-1]) - 100 == 0.0:
+            elif float(self.percentage(value, buy_or_sell)[1][0:-1]) - 100 == 0.0:
                 return (
                     f"Стоимость [{self.get_coin_name(self.coin_first)}] не изменилась "
                     f"{self.coin_trade_info(buy_or_sell)[-1]} [{self.get_coin_name(self.coin_second)}]"
@@ -40,7 +62,7 @@ class DataAnalysis(DataBase):
                 return (
                     f"Изменение стоимости [{self.get_coin_name(self.coin_first)}] по отношению к "
                     f"[{self.get_coin_name(self.coin_second)}] {self.coin_trade_info(buy_or_sell)[-1]} "
-                    f"составило: {float(percent[0:-1]) - 100:.8f}% ↑"
+                    f"составило: {float(self.percentage(value, buy_or_sell)[1][0:-1]) - 100:.8f}% ↑"
                 )
         else:
             return (
@@ -67,7 +89,7 @@ class DataAnalysis(DataBase):
             f"Комиссия составляет: {percent}%\n"
             f"Начальный результат в {self.get_coin_name(self.coin_second)}: {start_price_minus_percent:.8f}\n"
             f"Актуальный результат в {self.get_coin_name(self.coin_second)}: {current_price_minus_percent:.8f}\n"
-            f"Изменение суммы: {current_price_minus_percent - start_price_minus_percent:.8f}\n"
+            f"Изменение суммы: {current_price_minus_percent - start_price_minus_percent:.8f}{self.buy_sell_verify()}\n"
             f"(Покупка) {self.get_exchange_info(self.coin_trade_info(self.buy)[-1], self.buy)}\n"
             f"(Продажа) {self.get_exchange_info(self.coin_trade_info(self.sell)[-1], self.sell)}\n"
         )
